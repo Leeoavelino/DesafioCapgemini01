cc44ec7a6ba4dbfe0c75b8331c4d4f46724166ac
Leonardo Avelino <leoave46@gmail.com>
2022-02-20 22:15:31 -0300

    DesafioCapgemini01
    Questão 01 do desafio da Capgemini 2022.

---
+++ questao001/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace questao001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o numero de linhas da escada: ");
+
+            var linha = int.Parse(Console.ReadLine());
+            var resultado = new StringBuilder();
+
+            for(int i = 1; i <= linha; i++)
+            {
+                string linhaAtual = string.Empty;
+                for (int j = 0; j < i; j++)
+                    linhaAtual += '*';
+
+                resultado.AppendLine(linhaAtual.PadLeft(linha, ' '));
+            }
+
+            Console.WriteLine(resultado.ToString());
+
+
+            Console.WriteLine("Obrigado!");
+
+        }
+    }
+}
